011b4c2803a5549410f67a193f192edcd416e460
GiuliaSossai <giulia.sossai@yahoo.it>
2021-11-10 18:15:58 +0100

    prove per timing function - non parte il prompt

--- js/script.js
+++ js/script.js
@@ -9,38 +9,48 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 *
  */
 
+/*****************
+  inizializzo variabili
+*****************/
+
 let myNumbers = [];
-//[getRandomInt(1, 100), getRandomInt(1, 100), getRandomInt(1, 100), getRandomInt(1, 100), getRandomInt(1, 100)];
 console.log('array da riempire', myNumbers);
 
-for (let i = 0; i < 5; i++){
-  console.log ('i', i);
-  let number = getRandomInt(1, 100);
-  //controllo unicità numero da pushare nell'array
-  if(!myNumbers.includes(number)){
-    myNumbers.push(number);
-  }
-  console.log('array - ciclo', myNumbers);
-}
-console.log('array riempito con 5 numeri', myNumbers);
-
-/**
- * 1. quando stampo l'array, devo far partire il countdown
- * 2. finito il countdown deve esserci un prompt che chiede all'utente di inserire i numeri visti 
- */
-
+const boxText = document.querySelector('.text');
+let numeriIndovinati = [];
 //inizializzo countdown
 let tempoSecondi = 5;
 
 
-const clock = setInterval(function() {
+/*****************
+  logica e funzioni
+*****************/
+
+/* TIMING FUNCTIONS
+
+ * 1. quando stampo l'array, devo far partire il countdown
+ * 2. finito il countdown deve esserci un prompt che chiede all'utente di inserire i numeri visti 
+ */
+//arrow function per countdown
+const countdown = setInterval(function() {
   //stampo l'array di 5 numeri random
   document.querySelector('.box').innerHTML = myNumbers;
+  boxText.innerHTML = `memorizza questi numeri, hai ${tempoSecondi} secondi`;
   tempoSecondi--;
-  console.log(tempoSecondi);
+  console.log(tempoSecondi);  
+}, 1000);
 
-  if (tempoSecondi === 0){
-    clearInterval(clock);
+
+setTimeout(function() {
+  boxText.innerHTML = `ti ricordi tutti i numeri?`;
+  clearInterval(countdown);
+}, tempoSecondi * 1000);
+
+
+setTimeout(function() {
+  if(tempoSecondi === 0){
+    boxText.innerHTML = `scrivi i numeri che ti ricordi`;
+    //clearInterval(clock);
     let userNumbers = prompt('scrivi i numeri che ricordi!');
     console.log('numeri scritti da utente:', userNumbers);
 
@@ -59,15 +69,11 @@ const clock = setInterval(function() {
     //confronto i due array usando ciclo
     /*se myNumbers include elementi di arrUser:
      1. aumento il counter
-     2. stampo in pagina
-     se nessun elemento è presente, counter = 0 e stampo msg 
+     2. stampo messaggio diverso a seconda del counter
     */
     
-    const boxText = document.querySelector('.text');
-    let numeriIndovinati = [];
-
-    // myNumbers -> [1, 3, 5, 7, 9]
-    // arrUser -> [1, 3, 5, 7, 10]
+    // const boxText = document.querySelector('.text');
+    // let numeriIndovinati = [];
 
     for (let i = 0; i < arrUser.length; i++){
       //se myNumbers include un elemento di arrUser, aumento il contatore
@@ -80,6 +86,7 @@ const clock = setInterval(function() {
     }
    console.log('numeri indovinati', numeriIndovinati);
 
+    //stampo messaggio output
     if(counter == 0){
       boxText.innerHTML = `
       <p>hai indovinato ${counter} numeri! ritenta</p>
@@ -89,11 +96,20 @@ const clock = setInterval(function() {
         <p>hai indovinato ${counter} numeri! ${numeriIndovinati}</p>
         `;
     }
-     
   }
-  
-}, 1000);
+}(tempoSecondi + 1) * 1000)
 
+//ciclo per formare arrray di 5 numeri random
+for (let i = 0; i < 5; i++){
+  console.log ('i', i);
+  let number = getRandomInt(1, 100);
+  //controllo unicità numero da pushare nell'array
+  if(!myNumbers.includes(number)){
+    myNumbers.push(number);
+  }
+  console.log('array - ciclo', myNumbers);
+}
+console.log('array riempito con 5 numeri', myNumbers);
 
 function getRandomInt(min, max){
   return Math.floor(Math.random() * (max - min) + min);
